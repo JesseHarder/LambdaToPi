f752ef99562aac6307ae8429086b8779f4b9d6fb
JesseHarder <jhharder@scu.edu>
2016-03-07 15:25:18 -0800

    Updates to translator to have iterator applied to new channels during the generation that they are created instead of the generation that they are passed to. Other small fixes as well.

--- jison/translator.js
+++ jison/translator.js
@@ -9,11 +9,11 @@ var iter = 0; /* global variable to keep track of iterations and be used for cha
  * Jesse's note: This function is an alternative idea I had. Not necessarily needed.
  */
 
-/* function newChannel () {
-    var channel = "chan".concat(iter.toString());
+function newChannel () {
+    var channel = "chanP".concat(iter);
     iter++;
     return channel;
-}*/
+}
 
 /* Function Name: translate
  * Input Parameters: (expr, chan), takes parser output array, and optional channel name
@@ -22,11 +22,15 @@ var iter = 0; /* global variable to keep track of iterations and be used for cha
 
 function translate (expr, chan) {
     iter++;
-    if (chan === undefined) chan = "chanP";
+    
+    /* Handling the case where channel name is not given. */
+    if (chan === undefined) chan = newChannel();
+    
     /* else chan = chan.concat(iter); }   not sure about this */
     
     var testvalue = expr[0]; /* first element of the input array */
     var varname;
+    var returnString;
     
     switch (testvalue) {
             
@@ -36,13 +40,13 @@ function translate (expr, chan) {
          *  1) The name of the variable as a string.
          *  Example: ["VarExpr", varname ]
          *
-         * Return: varname!chan;
+         * Return String: varname!chan;
          * if channel input is provided, it is passed to chan, if not, it is returned as chan default
          */
     
         case "VarExpr":
             varname = expr[1]; /* second element of the input array */
-            return varname.concat("!", chan);
+            returnString = varname.concat("!", chan);
             
         
             
@@ -53,14 +57,15 @@ function translate (expr, chan) {
          *  2) A sub-expression.
          *  Example: ["LambdaExpr", varname, expr]
          *
-         * Return: chan?varname.chan?chanQ.translate(childexpr, chanQ);
+         * Return String: chan?varname.chan?chanQ.translate(childexpr, chanQ);
          * need a recursive call to translate the inner expression
          */
             
         case "LambdaExpr":
             varname = expr[1]; /* second element of the input array */
             var childexpr = expr[2]; /* third element of the input array */
-            return chan.concat("?", varname, " . ", chan, "?", ("chanQ").concat(iter), " . ", translate(childexpr, ("chanQ").concat(iter)));
+            var chanQ = ("chanQ").concat(iter)
+            returnString = chan.concat("?", varname, " . ", chan, "?", chanQ, " . ", translate(childexpr, chanQ));
             
         
             
@@ -71,7 +76,7 @@ function translate (expr, chan) {
          *  2) The second expression. The one being applied to the first.
          *  Example: ["ApplyExpr", expr1, expr2]
          *
-         * Return: new(chanA).new(chanB).(translate([expr1],(chanA))|(chanA!chanB.chanA!chan)|*((chanB?chanC).translate([expr2],(chanC)));
+         * Return String: new(chanA).new(chanB).(translate([expr1],(chanA))|(chanA!chanB.chanA!chan)|*((chanB?chanC).translate([expr2],(chanC)));
          *
          * Two recursive calls needed to translate the two expressions.
          */
@@ -79,7 +84,13 @@ function translate (expr, chan) {
         case "ApplyExpr":
             var expr1 = expr[1]; /* second element of the input array */
             var expr2 = expr[2]; /* third element of the input array */
+            var chanA = "chanA".concat(iter);
+            var chanB = "chanB".concat(iter);
+            var chanC = "chanC".concat(iter);
+            
             /* chanA and chanB should also be concatenated with iter */
-            return ( "new(chanA).new(chanB) . (" ).concat(translate([expr1], "chanA"), " | (chanA!chanB . chanA!", chan , ") | *(( chanB?chanC . ", translate([expr2], "chanC"), ")");
+            returnString = ( "new(" ).concat(chanA,").new(",chanB,") . (",translate([expr1], chanA), " | (",chanA,"!",chanB," . ",chanA,"!", chan , ") | *(( ",chanB,"?",chanC," . ", translate([expr2], chanC), ")");
             }
+    
+    return returnString;
 }
